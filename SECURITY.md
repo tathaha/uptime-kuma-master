d6614efa0c8d4d3d9320f337be2e3059ef83ae3c
Chiraitori <77224364+tathaha@users.noreply.github.com>
2022-05-14 17:29:48 +0700

    Initial commit

---
+++ SECURITY.md
@@ -0,0 +1,28 @@
+# Security Policy
+
+## Reporting a Vulnerability
+
+Please report security issues to uptime@kuma.pet.
+
+Do not use the issue tracker or discuss it in the public as it will cause more damage.
+
+## Supported Versions
+
+Use this section to tell people about which versions of your project are
+currently being supported with security updates.
+
+### Uptime Kuma Versions
+
+You should use or upgrade to the latest version of Uptime Kuma. All `1.X.X` versions are upgradable to the lastest version.
+
+### Upgradable Docker Tags
+
+| Tag | Supported          |
+| ------- | ------------------ |
+| 1 | :white_check_mark: |
+| 1-debian | :white_check_mark: |
+| latest | :white_check_mark: |
+| debian | :white_check_mark: |
+| 1-alpine | ⚠️ Deprecated |
+| alpine | ⚠️ Deprecated |
+| All other tags  | ❌ |
---
+++ src/components/Datetime.vue
@@ -0,0 +1,36 @@
+<template>
+    <span>{{ displayText }}</span>
+</template>
+
+<script>
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import timezone from "dayjs/plugin/timezone"; // dependent on utc plugin
+import utc from "dayjs/plugin/utc";
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.extend(relativeTime);
+
+export default {
+    props: {
+        value: {
+            type: String,
+            default: null,
+        },
+        dateOnly: {
+            type: Boolean,
+            default: false,
+        },
+    },
+
+    computed: {
+        displayText() {
+            if (this.dateOnly) {
+                return this.$root.date(this.value);
+            } else {
+                return this.$root.datetime(this.value);
+            }
+        },
+    },
+};
+</script>
